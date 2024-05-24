92937a6f70ed103fb45a1116c438434f14f563e6
mohammadali202356 <mohammadali202356@gmail.com>
2024-05-24 14:59:36 +0600

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,22 @@
+mark = 100
+if mark >= 33:
+   print("Pass")
+else:
+   print("Fail")
+
+
+
+num = 100 
+if num >200:
+    print("num is greater than 50")
+else:
+    print("num is not greater than 50")
+    
+    
+    
+num1 = 15
+num2 = 25
+if num1<num2:  
+  print (num1)
+else:
+  print (num2)
