980bc770c9569ab39315cf45158bb818d84f92d7
mohammadali202356 <mohammadali202356@gmail.com>
2024-05-24 14:58:36 +0600

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,31 @@
+name = "MD"
+age = 17
+iq = 250.77
+result ="Name: " + name + " ,age: " +str(age)+ " ,iq: " +str(iq) 
+print (result)
+
+
+Data1 = "khalid"
+Data2 = "nirob"
+age1 = 17
+age2 = 20
+height1 = 5.5
+height2 = 6.3
+result1 = "There are two friends , Name " +Data1 + " and " +Data2 + ".They are " +str(age1) + " and " + str(age2) + " years old."
+"Their height is " + str(height1) + "and " + str(height2)
+print(result1)
+
+
+name = "orio"
+age = 17
+height = 6.76333
+result = "Name:%s, Age:%d, Height:%.2f" % (name,age,height) 
+print(result)
+
+
+
+name1 = "Mohammad"
+age1 = 18
+speed = 31.3465
+result = "Name: %s, Age: %d, Running speed: %.2f" % (name1,age1,speed)
+print(result)
